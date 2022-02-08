f1ca172bddb0767970e5a2da149722c358771832
rproft <rproft@student.umb.sk>
2022-02-08 15:28:44 +0100

    pridana trieda Kniznica

---
+++ src/Kniznica.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kniznica {
+    List<Kniha> zoznamKnih = new ArrayList<>();
+
+    void pridajKnihu() {
+
+    }
+    void vypisKnih() {
+
+    }
+}
