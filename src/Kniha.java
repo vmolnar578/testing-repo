6c03b6368b2c8b8d26102e535c89068b509c1010
vmolnar <vmolnar@student.umb.sk>
2022-02-08 15:23:13 +0100

    pridana trieda kniha

---
+++ src/Kniha.java
@@ -0,0 +1,11 @@
+public class Kniha {
+    private String autor;
+    private String nazov;
+    private int pocetStran;
+
+    public Kniha(String autor, String nazov, int pocetStran) {
+        this.autor = autor;
+        this.nazov = nazov;
+        this.pocetStran = pocetStran;
+    }
+}
