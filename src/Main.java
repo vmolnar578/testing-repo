c2abb53a706f9e11f16dede83cc5ad458204f52e
vmolnar <vmolnar@student.umb.sk>
2022-02-08 15:38:37 +0100

    dokoncenie pridania a vypisu knih

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Kniha k1 = new Kniha("J.K. Rowling", "Harry Potter", 354);
+        Kniznica kn1 = new Kniznica();
+        kn1.pridajKnihu(k1);
+        kn1.vypisKnih();
     }
 }
